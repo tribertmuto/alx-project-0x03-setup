2fb3e48c20167515724c91ce617b8de76349176e
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-27 23:28:27 +0200

    changes

---
+++ alx-project-0x03-setup/interface/index.ts
@@ -0,0 +1,15 @@
+
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+
+export interface PageRouteProps {
+  pageRoute: string
+}
+
+export interface LayoutProps {
+  children: React.ReactNode
+}
