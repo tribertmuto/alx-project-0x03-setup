00ecab072342687d821307dda032e7f5bb9228de
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-28 00:18:18 +0200

    changes

---
+++ alx-project-0x03/interface/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+
+export interface PageRouteProps {
+  pageRoute: string
+}
+
+export interface LayoutProps {
+  children: React.ReactNode
+} 
