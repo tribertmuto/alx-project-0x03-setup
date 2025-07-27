229f0e7f1ad40d9e3366fe134a4aadc8d533d388
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-27 23:59:30 +0200

    changes

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,10 +1,7 @@
 
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-
-interface PageRouteProps {
-  pageRoute: string
-}
+import { PageRouteProps } from "@/interface";
 
 export default function Home() {
   const router = useRouter()
@@ -15,7 +12,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to Splash App!</h1>
+      <h1> Welcome to Splash App </h1>
       <p className="text-lg text-gray-600 mb-8">Your one-stop platform for everything AI you need.</p>
       <div className="flex gap-6">
         <Button action={() => routeToNextPage({ pageRoute: '/generate-text-ai' })} buttonLabel="Generate Text" buttonBackgroundColor="blue" />
--- alx-project-0x03/src/app/not-found.tsx
+++ alx-project-0x03/src/app/not-found.tsx
@@ -7,12 +7,12 @@ const Custom404 = () => {
       {/* Main Heading */}
       <h1 className="text-6xl font-bold mb-4">Oops! 😱</h1>
       <p className="text-2xl mb-8">
-        We can't seem to find the page you're looking for.
+        We can&apos;t seem to find the page you&apos;re looking for.
       </p>
 
       {/* Funny Message */}
       <p className="text-lg mb-8 text-center max-w-md">
-        Maybe it was abducted by aliens 👽, or it just took a wrong turn into the Internet wilderness! Either way, it's not here.
+        Maybe it was abducted by aliens 👽, or it just took a wrong turn into the Internet wilderness! Either way, it&apos;s not here.
       </p>
 
       {/* Button to navigate back */}
