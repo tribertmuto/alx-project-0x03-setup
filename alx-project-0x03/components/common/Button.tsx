8534d9571acb83fd051b02fcdc86f8dff9176384
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-28 00:36:35 +0200

    changes

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,18 +1,20 @@
+import { ButtonProps } from "../interface";
 
-interface ButtonProps {
-  buttonLabel: string
-  buttonSize?: string
-  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
-  action?: () => void
-}
-
-const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
-  const backgroundColorClass = buttonBackgroundColor ? {
+const Button = ({
+  buttonLabel,
+  buttonSize,
+  buttonBackgroundColor,
+  action,
+}: ButtonProps) => {
+  const colorMap: Record<'red' | 'blue' | 'orange' | 'green', string> = {
     red: 'bg-red-500',
     blue: 'bg-blue-500',
     orange: 'bg-orange-500',
     green: 'bg-green-500',
-  }[buttonBackgroundColor] : 'bg-slate-500'
+  };
+  const backgroundColorClass = buttonBackgroundColor && colorMap[buttonBackgroundColor as keyof typeof colorMap]
+    ? colorMap[buttonBackgroundColor as keyof typeof colorMap]
+    : 'bg-slate-500';
 
   return (
     <button onClick={action} className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg hover:${backgroundColorClass}/50 transition duration-300 text-white`}>
